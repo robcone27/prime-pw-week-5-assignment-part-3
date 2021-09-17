a4a06dbda0eb84fe99f04854b00351e3e09ece83
Rob Cone <robcone86@gmail.com>
2021-09-17 08:44:38 -0500

    repoupdate

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,46 @@
-console.log('***** Music Collection *****')
+
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+let album = {
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished,
+}
+collection.push(album);
+return album;
+}
+addToCollection( 'Aspiration', 'After The Burial', 2008 );
+addToCollection( 'Bezerker', 'After The Burial', 2008 );
+addToCollection( 'Master of Puppets', 'Metallica', 1988 );
+addToCollection( 'The World Is Yours', 'Nas', 1993 );
+addToCollection( 'Black Ice', 'Goodie Mob', 2007 );
+addToCollection( 'Kick In The Door', 'Notorious BIG', 1997 );
+
+console.log(collection);
+
+// console.log(collection[0].title);
+
+// for( let i = 0; i < collection.length; i++) {
+// console.log(collection[i].title);
+// }
+
+function showCollection(albums){
+  for( let i = 0; i < albums.length; i++) {
+  console.log('TITLE:', albums[i].title, 'ARTIST:',
+  albums[i].artist,'RELEASE DATE:', albums[i].yearPublished );
+ }
+}
+console.log(showCollection(collection) );
+
+function findByArtist(artist){
+let found = [];
+for(let i in collection) {
+if(artist === collection[i].artist){
+  found.push(collection[i]);
+  }
+ } return console.log('albums found: ', found.length, found);
+};
+
+findByArtist('Metallica');
